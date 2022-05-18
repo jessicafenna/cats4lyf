3a381493926486e522438da952b2767d404df48a
Jessica Fenna <jessicafenna@gmail.com>
2022-05-18 12:37:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import {  useEffect, useState } from 'react';
+const { faker } = require('@faker-js/faker');
+// import Modal from './components/modal';
+
+ 
+  const App = () => {
+    const [zooAnimal, setZooAnimal] = useState([]);
+    
+    
+    const fetchData = async () => {
+      
+      try {
+  
+        const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+  
+      
+        if(!response.ok){
+          throw new Error(response.statusText)
+        }
+     
+        const data = await response.json();
+  
+        console.log(data)
+        setZooAnimal(data)
+      } catch (err) {
+        
+        console.log(err)
+      }
+    }
+    
+   
+    useEffect(() => {
+      
+      fetchData()
+  
+    }, [])
+  
+    return (
+      <div className="App">
+        <h1>Cats</h1>
+        {zooAnimal.map((data, index) => {
+          // map through API data stored in the state and display it to the user
+          return (
+            <div key={index}>
+              <h3>Name: {faker.name.findName()}</h3>
+              <img src={data.url} alt="film"/>              
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+  
 
 export default App;
