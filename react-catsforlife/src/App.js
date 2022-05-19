10bc5a4b7638785ec7b8f32c5c6c0a5f5db828dc
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-05-19 11:46:20 +0100

    Add files via upload

---
+++ react-catsforlife/src/App.js
@@ -0,0 +1,54 @@
+import './styling.css'
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  VStack,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+import { CatApp } from './components/CatApp';
+// import Cart from './components/cart/Cart'
+
+
+function App() {
+
+  // useEffect(() => {
+  //   commerce.cart.retrieve()
+  //     .then(cart => {
+  //       setCart(cart);
+  //     })
+  // }, [])
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="center" hidden/>
+          
+          <div></div>
+          <div></div>
+          <div></div>
+
+          <VStack spacing={3}>
+          <div className='main-text'>
+            <h1 id='heading-main'>CATS4LYFE<span id='heading-split'><span className='letters-b'>.b</span><span><ColorModeSwitcher className="color-switcher" justifySelf="center"/></span>nk</span></h1>
+            <p id='paragraph-main'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+
+            <div></div>
+            <div></div>
+
+            <div className='cat-app'>
+              <CatApp />
+            </div>
+
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
