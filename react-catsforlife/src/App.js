a636513f4749e484a6b064f88241ba2b8910f5b9
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-05-20 15:40:40 +0100

    Update App.js

--- react-catsforlife/src/App.js
+++ react-catsforlife/src/App.js
@@ -25,7 +25,7 @@ function App() {
           <VStack spacing={3}>
           <div className='main-text'>
             <h1 id='heading-main'>CATS4LYFE<span id='heading-split'><span className='letters-b'>.b</span><span><ColorModeSwitcher className="color-switcher" justifySelf="center"/></span>nk</span></h1>
-            <p id='paragraph-main'>Blessings of the moons upon you, traveller! Thanks for stopping by at <span className='paragraph-split'>CATS4LYFE</span>. We all know why you are here, your obsession for cats has proven yourself worthy? These creatures of the Desert are looking for a home. They however <span className='paragraph-split'>DO NOT</span> come with warranty so feel free to peel of the sticker when you recieve said Feline. I want to wish you the best on your choice of <code className='code-bit'>SPECIMEN</code>, I hope you will find a warmer welcome in your travels than we have found in ours.</p>
+            <p id='paragraph-main'>Blessings of the moons upon you, traveller! Thanks for stopping by at <span className='paragraph-split'>CATS4LYFE</span>. We all know why you are here, your obsession for cats has proven yourself worthy? These creatures of the Desert are looking for a home. They however <span className='paragraph-split'>DO NOT</span> come with warranty so feel free to peel of the sticker when you recieve said Feline. I want to wish you the best on your choice of <code className='code-bit'>KHAJIIT</code>, I hope you will find a warmer welcome in your travels than we have found in ours.</p>
           </div>
 
             <div></div>
